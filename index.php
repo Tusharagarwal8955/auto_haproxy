952c6200ae055bf7b79d81b48eb9b16a3ed12564
Tusharagarwal8955 <agrawal.edu800@gmail.com>
2021-04-07 04:31:56 -0400

    final commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<body bgcolor =red>
+
+<h1 color= white align= center >Load Balancer using Ansible</h1>
+
+<pre>
+<?php
+print "this is automated haproxy using ansible";
+print "<br>";
+print `/usr/sbin/ifconfig`;
+?>
+</pre>
